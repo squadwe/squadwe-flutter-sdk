49a884f3c451d8b3fa7690dbce847e8baf988d81
joshorug <josh@squadwe.com>
2022-12-25 13:59:25 -0300

    client_sdk

---
+++ lib/squadwe_client_sdk.dart
@@ -0,0 +1,9 @@
+library squadwe_client_sdk;
+
+export 'squadwe_callbacks.dart';
+export 'squadwe_client.dart';
+export 'package:squadwe_client_sdk/ui/squadwe_chat_page.dart';
+export 'package:squadwe_client_sdk/ui/squadwe_chat_dialog.dart';
+export 'package:squadwe_client_sdk/data/local/entity/squadwe_message.dart';
+export 'package:squadwe_client_sdk/data/local/entity/squadwe_user.dart';
+export 'package:squadwe_client_sdk/data/remote/squadwe_client_exception.dart';
